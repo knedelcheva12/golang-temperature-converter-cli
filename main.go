a03d64f821187d5a96db0fafb5bcabe30b0fadf3
Kristiana <kristiana.nedelcheva@sap.com>
2022-08-05 16:52:18 +0300

    wip

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -19,6 +20,17 @@ var errReadingInput = errors.New("Error reading input")
 func main() {
 
 	for {
+		lengthOfArgs := len(os.Args)
+
+		if lengthOfArgs != 2 {
+			printError(errInvalidArguments)
+		}
+
+		s1 := strings.ToUpper(os.Args[1])
+		fmt.Println(s1)
+
+		originUnit = s1
+
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
